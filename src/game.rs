24a2fdfae19af4378d124e9ed47b48206fec3720
zanderalbaz <119557980+zanderalbaz@users.noreply.github.com>
2024-12-03 17:39:57 -0700

    forgot to push these with PlayerBundle commit

--- src/game.rs
+++ src/game.rs
@@ -3,12 +3,13 @@ pub mod systems;
 pub mod plugins;
 pub mod setup;
 pub mod constants;
+pub mod bundles;
 
 use bevy::prelude::*;
 use constants::AppState;
 use plugins::StartupPlugin;
 use setup::{setupScreen_setup, start_setup};
-use systems::{start_game, setup_screen};
+use systems::{setup_screen, spawn_test_player, start_game, test_player_balance_change};
 
 #[derive(SystemSet, Debug, Hash, PartialEq, Eq, Clone)]
 pub struct StartGameSystemSet;
@@ -25,6 +26,8 @@ pub fn run(){
 	.add_systems(Update, start_game.in_set(StartGameSystemSet))
 	.add_systems(Update, setupScreen_setup.in_set(SetupGameSystemSet).run_if(in_state(AppState::Setup).and_then(run_once())))
 	.add_systems(Update, setup_screen.in_set(SetupGameSystemSet).run_if(in_state(AppState::Setup).and_then(run_once())))
+	.add_systems(Startup, spawn_test_player)
+	.add_systems(Update, test_player_balance_change)
 	.add_plugins(StartupPlugin)
 	.insert_state(AppState::Start)
 	
