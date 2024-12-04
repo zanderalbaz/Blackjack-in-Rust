abaef17bb344811930a18c1a4dfa5aa28257ac10
azamora96 <alec.zamora@western.edu>
2024-12-03 19:09:29 -0700

    dealer side static gui complete

--- src/game/systems.rs
+++ src/game/systems.rs
@@ -371,6 +371,95 @@ pub fn inGame_setup(mut commands: Commands, assets: Res<AssetServer>) {
             },
             ..default()
         });
+    })
+    .with_children(|parent| {
+        parent.spawn(TextBundle {
+            text: Text {
+                sections: vec![
+                    TextSection {
+                        value: "Dealer".to_string(),
+                        style: TextStyle {
+                            font: assets.load("fonts/FiraSans-SemiBold.ttf"), 
+                            font_size: 30.0,  
+                            color: Color::WHITE,  
+                        },
+                    },
+                ],
+                ..default()
+            },
+            style: Style {
+                position_type: PositionType::Absolute,
+                left: Val::Px(415.0),  
+                top: Val::Px(15.0),
+                ..default()
+            },
+            ..default()
+        });
+    })
+    .with_children(|parent| {
+        
+        parent.spawn(ImageBundle {
+            style: Style {
+                width: Val::Px(150.0), 
+                height: Val::Px(195.0),
+                position_type: PositionType::Absolute,
+                left: Val::Px(450.0),  
+                top: Val::Px(100.0),   
+                ..default()
+            },
+            image: UiImage {
+                texture: assets.load("deck/card_back.png"),  
+                ..default()
+            },
+            ..default()
+        });
+    })
+    .with_children(|parent| {
+        
+        parent.spawn(ImageBundle {
+            style: Style {
+                width: Val::Px(150.0), 
+                height: Val::Px(195.0),
+                position_type: PositionType::Absolute,
+                left: Val::Px(605.0),  
+                top: Val::Px(100.0),   
+                ..default()
+            },
+            image: UiImage {
+                texture: assets.load("deck/card_back.png"),  
+                ..default()
+            },
+            ..default()
+        });
+    })
+    .with_children(|parent| {
+        parent.spawn(ButtonBundle {
+            style: Style {
+                width: Val::Px(90.0),
+                height: Val::Px(50.0),
+                position_type: PositionType::Absolute, 
+                left: Val::Px(690.0),   
+                top: Val::Px(10.0),  
+                border: UiRect::all(Val::Px(5.0)),
+                justify_content: JustifyContent::Center,
+                align_items: AlignItems::Center,
+                margin: UiRect {left: Val::Px(10.0), bottom: Val::Px(10.0), ..default()},
+                ..default()
+
+            },
+            border_color: BorderColor(Color::BLACK),
+            background_color: BackgroundColor(Color::srgb(0.15, 0.15, 0.15)),
+            ..default()
+        })
+        .with_children(|parent| {
+            parent.spawn(TextBundle::from_section("Home",
+             TextStyle {
+                font: assets.load("fonts/FiraSans-SemiBold.ttf"),
+                font_size: 15.0,
+                color: Color::srgb(0.9, 0.9, 0.9),
+             }
+            ));
+        });
     });
     
 }
